0de28ac48ff707660510be437fb8ffff8f1c9a4b
Vivek Prasad <vivekpsd01@gmail.com>
2021-05-12 00:58:49 +0530

    Deployment Bug Fixed

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -5,41 +5,44 @@ import { FaCheck } from 'react-icons/fa';
 import Zoom from 'react-reveal/Zoom';
 
 export default function TodoItem(props) {
-  const items = props.items;
   return (
     <div className='todo-item-box'>
-      <Container fluid className='filter-box'>
-        Todo List
-        <Row className='justify-content-center '>
-          <Col md={6}>
-            <hr></hr>
-            {items.map((item) => {
-              return (
-                <Zoom duration={200}>
-                  <Card className='todo-item'>
-                    <span>
-                      <input
-                        type='text'
-                        value={item.task}
-                        className={item.class}
-                        onChange={() => props.setUpdate(item._id)}
-                      />
-                      <AiFillDelete
-                        className='del-icon'
-                        onClick={() => props.deleteItem(item._id)}
-                      />
-                      <FaCheck
-                        className='check-icon'
-                        onClick={() => props.checkedItem(item._id)}
-                      />
-                    </span>
-                  </Card>
-                </Zoom>
-              );
-            })}
-          </Col>
-        </Row>
-      </Container>
+      {props.items.task === undefined || props.items.task === '' ? (
+        <p>Loading...</p>
+      ) : (
+        <Container fluid className='filter-box'>
+          Todo List
+          <Row className='justify-content-center '>
+            <Col md={6}>
+              <hr></hr>
+              {props.items.map((item) => {
+                return (
+                  <Zoom duration={200}>
+                    <Card className='todo-item'>
+                      <span>
+                        <input
+                          type='text'
+                          value={item.task}
+                          className={item.class}
+                          onChange={() => props.setUpdate(item._id)}
+                        />
+                        <AiFillDelete
+                          className='del-icon'
+                          onClick={() => props.deleteItem(item._id)}
+                        />
+                        <FaCheck
+                          className='check-icon'
+                          onClick={() => props.checkedItem(item._id)}
+                        />
+                      </span>
+                    </Card>
+                  </Zoom>
+                );
+              })}
+            </Col>
+          </Row>
+        </Container>
+      )}
     </div>
   );
 }
