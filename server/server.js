21ea71e190ce3dfff4e9dd864ef595e5a9ffae57
Vivek Prasad <vivekpsd01@gmail.com>
2021-05-11 17:42:04 +0530

    MongoDB Connected

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+//Connect DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
